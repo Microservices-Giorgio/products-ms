0a44b1f8e14aabae26bb211ac31a5da65ec1e4da
Giorgio Ventura <gventuraagramonte@gmail.com>
2024-05-14 17:16:17 -0500

    Se agrego NATS y se cambio la estructura completa del codigo

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,15 +4,22 @@ import * as Joi from 'joi'
 interface EnvVars {
     PORT: number;
     DATABASE_URL: string;
+
+    NATS_SERVERS: string[]
 }
 
 const envsSchema = Joi.object({
     PORT: Joi.number().required(),
     DATABASE_URL: Joi.string().required(),
+    
+    NATS_SERVERS: Joi.array().items(Joi.string()).required()
 })
 .unknown(true)
 
-const {error, value} = envsSchema.validate(process.env)
+const {error, value} = envsSchema.validate({
+    ...process.env,
+    NATS_SERVERS: process.env.NATS_SERVERS?.split(',')
+})
 
 if(error){
     throw new Error(`Config validation error: ${error.message}`)
@@ -22,6 +29,8 @@ const envVars: EnvVars = value
 
 export const envs = {
     port: envVars.PORT,
-    databaseUrl: envVars.DATABASE_URL
+    databaseUrl: envVars.DATABASE_URL,
+    natsServers: envVars.NATS_SERVERS
+
 
 }
